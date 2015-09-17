b6be51c96f3459b124784ea00160a79c464942f8
Ricky <ricky@ricky-VirtualBox.(none)>
2015-09-17 13:31:04 +1000

    Configured project to be built by cmake

--- src/LegacyModes/Console.h
+++
@@ -1,7 +0,0 @@
-#include "Connections.h"
-#include "Printing.h"
-
-void updateAll(Siggy&, Connections&, SystemInfo&, std::string dbPath);
-void runInterface(int, char*);
-template<typename T>
-std::vector<T> split(const std::string& line);
---
+++ src/includes/Siggy.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#ifndef SIGGY_H
+#define SIGGY_H
+
+#include <iostream>
+#include <fstream>
+#include <boost/asio.hpp>
+#include <boost/asio/ssl.hpp>
+
+
+#include "sqlite3.h"
+#include "json_spirit.h"
+
+class Siggy
+{
+	std::string novaHeaderFilepath, theraHeaderFilepath;
+	std::string novaRequestData, theraRequestData;
+	std::wstring novaURL, theraURL;
+	json_spirit::mValue novaData, theraData;
+
+	std::string loadNovaRequest();
+	std::string loadTheraRequest();
+
+public:
+	Siggy();
+	void update();
+	json_spirit::mValue getData(std::string);
+	json_spirit::mValue getNovaData();
+	json_spirit::mValue getTheraData();
+};
+
+#endif
